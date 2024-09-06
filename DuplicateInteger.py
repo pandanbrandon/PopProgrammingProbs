1278e56647ba9df85c027a362f72419b560b87a2
beeteaa <itspandanbrandon@gmail.com>
2024-09-05 21:27:12 -0500

    Test email change

--- DuplicateInteger.py
+++ DuplicateInteger.py
@@ -1,6 +1,6 @@
 '''
     Given an integer array [nums], return true if any value appears more
-    than once in the array, otherwise return false.
+    than once in the array, otherwise return false. yuh
 '''
 
 # use 'count' method
